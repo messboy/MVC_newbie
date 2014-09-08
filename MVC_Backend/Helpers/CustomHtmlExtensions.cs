e00f66f2d14989ff50491dc69fcba382761d3b53
messboy home <messboy000@gmail.com>
2014-09-08 19:26:41 +0800

    後台:menu高亮顯示當前頁面

---
+++ MVC_Backend/Helpers/CustomHtmlExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Backend.Helpers
+{
+    public static class CustomHtmlExtensions
+    {
+        /// <summary>
+        /// 判斷是否為當前控制項頁面
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="controllerName"></param>
+        /// <returns></returns>
+        public static bool IsCurrentController(this HtmlHelper helper, string controllerName)
+        {
+            var currentControllerName =
+                (string) helper.ViewContext.RouteData.Values["controller"];
+
+            if (currentControllerName.Equals(controllerName, StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
