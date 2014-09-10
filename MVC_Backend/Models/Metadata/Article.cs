6d85d8182eeea157f1725ef6619a68e1b75c87e7
messboy home <messboy000@gmail.com>
2014-09-10 21:58:54 +0800

    Article metadata

---
+++ MVC_Backend/Models/Metadata/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Backend.Models
+{
+    [MetadataType(typeof(ArticleMetadata))]
+    public partial class Article
+    {
+        public class ArticleMetadata
+        {
+            [DataType(DataType.Html)]
+            public object Content { get; set; }
+
+            [UIHint("SystemUser")]
+            public object CreateUser { get; set; }
+
+            [UIHint("SystemUser")]
+            public object UpdateUser { get; set; }     
+        }
+    }
+}
